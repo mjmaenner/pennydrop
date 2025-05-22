0b898140a9113887e0a95a5c48983e26572ab48d
Matthew Maenner <maenner@gmail.com>
2025-05-22 11:16:29 -0400

    Update pennydrop.r
    revised to use a single function for different strategies. Fixed an issue with switching turns, added a list of future improvements.

--- pennydrop.r
+++ pennydrop.r
@@ -1,58 +1,32 @@
 # penny drop simulator
 
-
 roll <- function(){
   roller <- sample(1:6,1,replace=F)
   return(roller)
 }
 
-strategy5 <- function(gamestatus){
+strategy <- function(gamestatus){
   up.pennies <- sum(gamestatus[4:8] )
-  decision <- ifelse(up.pennies< 5, 1,0)  
+  strat<- if(gamestatus[1]==1){
+            gamestatus[10]
+          }else{
+            gamestatus[11]
+          }
+  decision <- ifelse(up.pennies< strat, 1,0)  
   return(decision)
 }
 
-strategy4 <- function(gamestatus){
-  up.pennies <- sum(gamestatus[4:8] )
-  decision <- ifelse(up.pennies< 4, 1,0)  
-  return(decision)
-}
-
-
-strategy3 <- function(gamestatus){
-  up.pennies <- sum(gamestatus[4:8] )
-  decision <- ifelse(up.pennies< 3, 1,0)  
-  return(decision)
-}
-
-strategy2 <- function(gamestatus){
-  up.pennies <- sum(gamestatus[4:8] )
-  decision <- ifelse(up.pennies< 2, 1,0)  
-  return(decision)
-}
-
-strategy1 <- function(gamestatus){
-  up.pennies <- sum(gamestatus[4:8] )
-  decision <- ifelse(up.pennies< 1, 1,0)  
-  return(decision)
-}
-
-decider <- function(gamestatus)
-  if(gamestatus[1]==1){
-    plan <- strategy2(gamestatus)
-  }else{
-    plan <- strategy3(gamestatus)
-  }
-
-pennydrop <- function(nplayers=2, npennies=10){
-  #gamestatus is a vector describing the status of the game
-  # it is defined as c(pturn, p1penny, p2penny, h1, h2, h3, h4, h5, bank) 
-  gamestatus <- c(1,10,10,0,0,0,0,0,0)
+pennydrop <- function(nplayers=length(strategies), npennies=10, strategies=c(3,3), printgame=FALSE){
+  # gamestatus is a vector describing the status of the game
+  # it is defined as c(pturn, p1penny, p2penny, h1, h2, h3, h4, h5, bank,strategy1, strategy2) 
+  gamestatus <- c(1,npennies,npennies,0,0,0,0,0,0,stratp1,stratp2)
   turns <- 1
+  playermove <- 1
 
   while(gamestatus[2]> 0 & gamestatus[3] > 0){
-  playermove <- 1
-  if(playermove ==1){
+  
+  
+  #if(playermove ==1){
     rollresult <- roll()
     
     if(gamestatus[rollresult+3]==0 & rollresult<6 ){
@@ -64,7 +38,7 @@ pennydrop <- function(nplayers=2, npennies=10){
       }else{
         gamestatus[3] <- gamestatus[3]-1
       }  
-    playermove <- decider(gamestatus)  
+      playermove <- strategy(gamestatus)  
       
       
     }else if(rollresult==6){
@@ -75,9 +49,9 @@ pennydrop <- function(nplayers=2, npennies=10){
         gamestatus[2] <- gamestatus[2]-1
       }else{
         gamestatus[3] <- gamestatus[3]-1
-      
-      playermove <- decider(gamestatus)  
       }
+      playermove <- strategy(gamestatus)  
+      
         
     }else{
       #bad roll
@@ -93,28 +67,46 @@ pennydrop <- function(nplayers=2, npennies=10){
      playermove = 0 
       
     }
-  }
+  #}
     #if they don't choose to roll, it defaults to pass, and they switch turns
-
-    # print(gamestatus)
-  
+    
+  if(printgame==TRUE){
+    print(gamestatus)
+  }
+    
     #switch turns
     if(playermove==0){
       gamestatus[1] <- ifelse(gamestatus[1]==1,2,1)
       turns <- turns +1
+      playermove <-1
     }
-  
+     
       }
 
 
-  
   winner <- ifelse(gamestatus[2]==0,1,2)
   return(c(winner,turns))
 }  
   
-games <- as.data.frame(t(replicate(20000,pennydrop(),simplify = T)))
+#games <- as.data.frame(t(replicate(20000,pennydrop(npennies=50,stratp1=3, stratp2=3),simplify = T)))
 
-gamesummary <- function(gameresult){
-  P1winner <- 
-  P2winner <- 
-}
+#games <- as.data.frame(t(replicate(20000,pennydrop(npennies=10,stratp1=3, stratp2=3),simplify = T)))
+
+
+#table(games$V1)
+
+#pennydrop(stratp1=2, stratp2=3, printgame=TRUE)
+
+
+
+
+# questions to ask
+# game length vs # starting pennies and # players - both individually and together
+# p1 advantage for pennies and players and strategies
+# do p1 and p2 wins have different game lengths?
+
+# average number of pennies up when rolling - vary by strategy
+# average outcome of successful rolls - vary by strategy
+
+# create a game summary that runs when a game is completed talking about win percents, game length, etc.
+# table(games$V1)
